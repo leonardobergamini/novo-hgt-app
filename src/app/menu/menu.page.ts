9835e5731e3a9428025f88448bbdbb757435bd02
Leonardo Bergamini <leonardobergamini@brq.com>
2019-06-21 17:12:07 -0300

    Criação do tab-menu

---
+++ src/app/menu/menu.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { getInjectableDef } from '@angular/core/src/di/defs';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+
+}
